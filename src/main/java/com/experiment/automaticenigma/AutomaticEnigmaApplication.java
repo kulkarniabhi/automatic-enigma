7f0b09c7c018f7f21ef2e7b0a74943743c09a099
AK <abhijeet.pk@gmail.com>
2021-06-26 22:36:09 +0530

    First build version.

---
+++ src/main/java/com/experiment/automaticenigma/AutomaticEnigmaApplication.java
@@ -0,0 +1,13 @@
+package com.experiment.automaticenigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticEnigmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomaticEnigmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/experiment/automaticenigma/AutomaticEnigmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experiment.automaticenigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaticEnigmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
